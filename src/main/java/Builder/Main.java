bf76eda9e87882643ef3425779ba3bc01efbe422
Rutwij <rutwij0507.rg@gmail.com>
2023-09-14 13:07:43 +0530

    Decorator Design Pattern

--- src/main/java/Builder/Main.java
+++ src/main/java/Builder/Main.java
@@ -12,13 +12,18 @@ public class Main {
                 .build();   //validate method will be called here
         System.out.println(student.toString());
 
+        /*problem Solved by Builder pattern:
+        Imagine a complex object that requires laborious, step-by-step initialization of many fields and
+        nested objects. Such initialization code is usually buried inside a monstrous constructor with lots
+        of parameters. Or even worse: scattered all over the client code.
 
-        //if validation gets successful then the student object gets created
-        //else it will throw the exceptions
-        //also follows OCP as you dont need to modify the code as in when if
-        // some of the attributes are not present in the object
 
-        /*
+        if validation gets successful then the student object gets created
+        else it will throw the exceptions
+        also follows OCP as you dont need to modify the code as in when if
+        some of the attributes are not present in the object
+
+
         Builder Design Pattern:
         1. Validate the Attributes before the obj gets created
         2. makes the Data & code Readable and Intuitive
